2f784461e0cca308eb2eb4903391b31ca892329b
Jacob Sulecki <slushi2018@gmail.com>
2023-08-03 20:42:26 -0500

    basic setup JS and how the functions will flow

---
+++ src/timer.js
@@ -0,0 +1,13 @@
+
+let startButton = document.getElementById('startTimer');
+let resetButton = document.getElementById('resetTimer');
+
+let timerId;
+
+function timerStart(){
+    console.log('time started')
+}
+
+function timerReset(){
+    console.log('time reset')
+}
