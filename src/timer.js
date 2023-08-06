e5db786b70dd2460712adaa4f25defdf7a5b7147
Jacob Sulecki <slushi2018@gmail.com>
2023-08-05 21:40:29 -0500

    set up JS to execute pompdoro intervals. todo: loop&count each intance until user reset.

--- src/timer.js
+++ src/timer.js
@@ -1,13 +1,39 @@
 
-let startButton = document.getElementById('startTimer');
-let resetButton = document.getElementById('resetTimer');
+const workStartingMinutes = 1;
+var workTime = workStartingMinutes * 60;
 
-let timerId;
-
-function timerStart(){
-    console.log('time started')
+function workCount(){
+    interval = setInterval(function updateWorkCountdown(){
+        let workMin = Math.floor(workTime / 60);
+        let workSec = workTime % 60;
+        workSec= workSec < 10 ? '0' + workSec : workSec;
+    
+        document.getElementById('workTimerCount').innerHTML = workMin + ' : ' + workSec;
+        workTime--;
+    
+        if ((workMin + workSec) <= 0){
+            clearInterval(interval);
+            restCount()
+         }
+    }, 1000);
 }
 
-function timerReset(){
-    console.log('time reset')
+
+const restStartingMinutes = 1;
+var restTime = restStartingMinutes * 60;
+
+function restCount(){
+    interval = setInterval(function updateRestCountdown(){
+        let restMin = Math.floor(restTime / 60);
+        let restSec = restTime % 60;
+        restSec= restSec < 10 ? '0' + restSec : restSec;
+    
+        document.getElementById('restTimerCount').innerHTML = restMin + ' : ' + restSec;
+        restTime--;
+    
+        if ((restMin + restSec) <= 0){
+            clearInterval(interval);
+         }
+    }, 1000);
 }
+
